b69e6c6ce837caa669c3114ae10bb1b8aa667b6b
ritalee79 <betrashy@gmail.com>
2017-08-11 20:49:25 +0200

    uiresources update

--- lib/UIresources.jsx
+++ lib/UIresources.jsx
@@ -1,134 +1,101 @@
-﻿var scriptTitle = "Search for layers in proj and do...";
+function createUserInterface (thisObj,userInterfaceString,scriptName){
+    var pal = (thisObj instanceof Panel) ? thisObj : new Window("palette", scriptName,
+								undefined,{resizeable: true});
+    if (pal == null) return pal;
 
-var myPalette = buildUI(this);
+    var UI=pal.add(userInterfaceString);
 
-if (myPalette != null && myPalette instanceof Window) {
-	myPalette.show();
+    pal.layout.layout(true);
+    pal.layout.resize();
+    pal.onResizing = pal.onResize = function () {
+        this.layout.resize();
+    }
+    if ((pal != null) && (pal instanceof Window)) {
+            pal.show();
 	}
+    return UI;
+};
 
-function buildUI (thisObject) {
 
-if (thisObject instanceof Panel) {
-	var myPalette = thisObject;
-	} else { 
-    var myPalette = new Window ("palette", scriptTitle, undefined, {resizeable:true});
-	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////// Main
 
-if (myPalette != null ) {
-	
-	var res =
-	"Group { \
-		orientation: 'column', \
-		alignment: ['fill','fill'], \
-		alignChildren: ['left','top'], \
-		searchTxt: StaticText {text:'Search string:'}, \
-		searchBox: EditText {alignment: ['fill','top']}, \
-		searchProj: RadioButton {text:'Search entire project'}, \
-		searchComps: RadioButton {text: 'Search selected comps only'},\
-		searchBtn: Button {text:'Search', alignment: ['right','top']}, \
-		}";
-	
-	myPalette.grp = myPalette.add(res);
-	myPalette.layout.layout(true);
-	myPalette.layout.resize();
-	
-	myPalette.grp.searchProj.value = true;
-	myPalette.grp.searchBtn.onClick = function () {
-		searchForLayer (myPalette);
-		}
-	
-	myPalette.onResizing = myPalette.onRize = function () {this.layout.resize();}
+var resourceString =
+"group{orientation:'column', alignment:['fill','fill'],alignChildren:['left','top']\
+    mainTextGroup: Group{orientation:'row',\
+        mainTextLabel:StaticText{text:'main text', preferredSize:[80,-1]},\
+        mainText: EditText{text:'enter main text here', characters:40}\
+    },\
+    secondaryTextGroup: Group{orientation:'row',\
+        secondaryTextLabel:StaticText{text:'secondary text', preferredSize:[80,-1]},\
+        secondaryText: EditText{text:'enter secondary text here', characters:40}\
+    },\
+    iconGroup: Group{orientation:'row',\
+        iconLabel:StaticText{text:'icon', preferredSize:[80,-1]},\
+        iconMamworld: RadioButton{text:'mamoworld', value:true},\
+        iconMochaImport: RadioButton{text:'MochaImport'},\
+        iconiExpressions: RadioButton{text:'iExpressions'}\
+        },\
+    colorGroup: Group{orientation:'row',\
+        colorLabel:StaticText{text:'color (RGB)', preferredSize:[80,-1]},\
+        redText: EditText{text:'243', characters:3},\
+        greenText: EditText{text:'151', characters:3},\
+        blueText: EditText{text:'27', characters:3},\
+    },\
+    fadeDurationGroup: Group{orientation:'row',\
+        fadeDurationLabel:StaticText{text:'fade duration', preferredSize:[80,-1]},\
+        fadeDurationText: EditText{text:'1.0', characters:3}\
+    },\
+	checkBoxSampleGrp: Group{orientation:'row',\
+        cbSample1Box: Checkbox {text:'sample1', value:true, preferredSize:[100,23]}\
+        cbSample2Box: Checkbox {text:'sample2'}\
+    },\
+    applyButton: Button{text:'Apply', alignment:['center', 'bottom']}\
+}";
 
-	} //if (myPalette != null ) 
-return myPalette;
-} //function buildUI (thisObject) 
 
-function searchForLayer (palObj) {
-	var searchString = palObj.grp.searchBox.text;
-	var searchProj = palObj.grp.searchProj.value;
-	//alert(searchProj);
-	
-	if (searchString == "") {
-		alert("Please enter a search string");
-		return;
-		}
-	
-	var myWin = new Window ("window", "Matching Layers", undefined, {resizeable:true});
-	
-	if (myWin != null) {
-		
-		var res = 
-		"Group { \
-		orientation: 'column', \
-		alignment: ['fill','fill'], \
-		alignChildren: ['left','top'], \
-			layersGrp: Group { \
-			alignment: ['fill','fill'], \
-			alignChildren: ['fill','fill'], \
-			layersTree: TreeView {} \
-			} \
-			optionsGrp: Group { \
-			orientation: 'column', \
-			alignment: ['fill','bottom'], \
-			alignChildren: ['fill','bottom'], \
-				deleteGrp: Group { \
-					deleteCheck: Checkbox {text:'Delete'} \
-				} \
-				enableGrp: Group { \
-					enableCheck: Checkbox {text:'Enable'} \
-					disableCheck: Checkbox {text:'Disable'} \
-				} \
-				renameGrp: Group { \
-					renameCheck: Checkbox {text:'Rename',alignment: ['left','top']} \
-					renameString: EditText {alignment: ['fill','top'], enabled:false} \
-				} \
-				propertyGrp: Group { \
-					propertyCheck: Checkbox {text:'Property',alignment: ['left','top']} \
-					propertyString: EditText {alignment: ['fill','top'], enabled:false} \
-					propertyLoadBtn: Button {text: 'Load',alignment: ['right','top'], enabled:false} \
-				} \
-				newValueGrp: Group { \
-					newValueTxt: StaticText {text:'New value:',alignment: ['left','top']} \
-					newValue0: EditText {alignment: ['fill','top']}, \
-					newValue1: EditText {alignment: ['fill','top']}, \
-					newValue2: EditText {alignment: ['fill','top']}, \
-					newValue3: EditText {alignment: ['fill','top']}, \
-					newValueUnits: StaticText {text:'units',alignment: ['right','top']} \
-				} \
-				doItGrp: Group { \
-					doItBtn: Button {text: 'Do It', alignment: ['right','top']} \
-				} \
-			} \
-		}"; 
-		
-		myWin.grp = myWin.add(res);
-		myWin.layout.layout(true);
-		myWin.layout.resize();
-		myWin.show();
-		palObj.hide();
-		
-		myWin.grp.optionsGrp.newValueGrp.visible = false;
-		myWin.grp.optionsGrp.newValueGrp.enabled = false;
-		
-		myWin.onClose = function () {
-			palObj.show();
-			}
-		myWin.onResizing = myWin.Resize = function () {this.layout.resize();}  //there was a typo on this line in the live class recording.  It said myWin.Rize when it should be myWin.Resize
-		
-		myWin.grp.optionsGrp.deleteGrp.deleteCheck.onClick = function () {
-				myWin.grp.optionsGrp.enableGrp.enabled = !this.value;
-				myWin.grp.optionsGrp.renameGrp.enabled = !this.value;
-				myWin.grp.optionsGrp.propertyGrp.enabled = !this.value;
-				myWin.grp.optionsGrp.newValueGrp.enabled = !this.value;
-			}
-		myWin.grp.optionsGrp.renameGrp.renameCheck.onClick = function () {
-			this.parent.renameString.enabled = this.value;
-			}
-		myWin.grp.optionsGrp.propertyGrp.propertyCheck.onClick = function () {
-			myWin.grp.optionsGrp.newValueGrp.visible = this.value;
-			myWin.grp.optionsGrp.propertyGrp.propertyString.enabled = this.value;
-			myWin.grp.optionsGrp.propertyGrp.propertyLoadBtn.enabled = this.value;
-			}
-		
-		} //if (myWin != null)
-	} //function searchForLayer ()
+var UI = createUserInterface(this,resourceString,"lower third script");
+
+UI.fadeDurationGroup.fadeDurationText.onChange = function(){
+    var myVal = parseFloat(UI.fadeDurationGroup.fadeDurationText.text);
+    if(isNaN(myVal)){
+        UI.fadeDurationGroup.fadeDurationText.text = 0;
+     }
+};
+
+UI.applyButton.onClick = function(){
+
+    var lowerThirdParameters = {
+        mainText : UI.mainTextGroup.mainText.text,
+        secondaryText : UI.secondaryTextGroup.secondaryText.text,
+        fillColor : [
+            parseFloat(UI.colorGroup.redText.text)/255,
+            parseFloat(UI.colorGroup.greenText.text)/255,
+            parseFloat(UI.colorGroup.blueText.text)/255
+            ],
+        fadeInDuration : parseFloat(UI.fadeDurationGroup.fadeDurationText.text),
+        fadeStartTime : 0,
+        icon: undefined
+        };
+
+    if(UI.iconGroup.iconMamworld.value){
+            lowerThirdParameters.icon = "mamoworld";
+    }
+    else if(UI.iconGroup.iconMochaImport.value){
+            lowerThirdParameters.icon = "mochaimport";
+    }
+    else if(UI.iconGroup.iconiExpressions.value){
+            lowerThirdParameters.icon = "iexpressions";
+    }
+    lowerThirdParameters.fadeEndTime = lowerThirdParameters.fadeStartTime+lowerThirdParameters.fadeInDuration;
+
+    ///////////// here follow the commands that do the job
+
+    var comp = app.project.activeItem;
+
+    if(comp instanceof CompItem){
+        adjustLowerThird(comp,lowerThirdParameters);
+    }
+    else {
+        alert("Please select a composition with lower third");
+    }
+}
